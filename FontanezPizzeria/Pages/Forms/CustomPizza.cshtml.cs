908bc392e755f6f4912962cada2b761e70a90382
William Fontanez <89318547+williamfontanez@users.noreply.github.com>
2022-12-15 10:46:17 -0600

    Created Pizza Model & Pizza View, Created Custom pizza form

--- FontanezPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ FontanezPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,9 +1,13 @@
+using FontanezPizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FontanezPizzeria.Pages.Forms;
 
 public class CustomPizza : PageModel
 {
+    [BindProperty]
+    public PizzasModel Pizza { get; set; }
     public void OnGet()
     {
         
